8bf0c6973ba30fc0f8cc380051e21bf7aaf0f988
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-10 22:29:42 +0200

    Update BrowserHistory.java

--- Advanced-2024/src/StacksAndQueues/BrowserHistory.java
+++ Advanced-2024/src/StacksAndQueues/BrowserHistory.java
@@ -1,12 +1,33 @@
 
-
 package StacksAndQueues;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class BrowserHistory {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+
+        ArrayDeque<String> history = new ArrayDeque<>();
+        while (!line.equals("Home")) {
+            if (line.equals("back") & !history.isEmpty()) {
+                String current = history.pop();
+                if(history.isEmpty()){
+                    System.out.println("no previous URLs");
+                    history.push(current);
+                }else {
+                    String newCurrent = history.pop();
+                    System.out.println(newCurrent);
+                    history.push(newCurrent);
+                }
+            } else {
+                System.out.println(line);
+                history.push(line);
+            }
+
+            line = sc.nextLine();
+        }
     }
 }
