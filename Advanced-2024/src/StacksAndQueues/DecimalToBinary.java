65fa0fa1b41bdb514e493e98f57a03ac72b9cc50
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-11 13:48:07 +0200

    Update DecimalToBinary.java

--- Advanced-2024/src/StacksAndQueues/DecimalToBinary.java
+++ Advanced-2024/src/StacksAndQueues/DecimalToBinary.java
@@ -8,13 +8,17 @@ public class DecimalToBinary {
         Scanner scanner = new Scanner(System.in);
 
         int num = Integer.parseInt(scanner.nextLine());
+        if (num == 0) {
+            System.out.println(0);
+            return;
+        }
         ArrayDeque<Integer> binary = new ArrayDeque<>();
         while (num != 0) {
             Integer current = num % 2;
             binary.push(current);
             num /= 2;
         }
-        while (!binary.isEmpty()){
+        while (!binary.isEmpty()) {
             System.out.print(binary.pop());
         }
     }
