62faf719c7b65dd32921ced13d77d50a41a44be9
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-11 11:09:35 +0200

    Create DecimalToBinary.java

---
+++ Advanced-2024/src/StacksAndQueues/DecimalToBinary.java
@@ -0,0 +1,21 @@
+package StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Integer> binary = new ArrayDeque<>();
+        while (num != 0) {
+            Integer current = num % 2;
+            binary.push(current);
+            num /= 2;
+        }
+        while (!binary.isEmpty()){
+            System.out.print(binary.pop());
+        }
+    }
+}
