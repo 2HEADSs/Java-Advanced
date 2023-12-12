bb0761b1b1fc24278ff234a7b0ce6aeac222c741
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-12 16:19:58 +0200

    Create ReverseNumberswithaStack.java

---
+++ Advanced-2024/src/StackAndQueuesExercise/ReverseNumberswithaStack.java
@@ -0,0 +1,15 @@
+package StackAndQueuesExercise;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class ReverseNumberswithaStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] input =  scanner.nextLine().split("\\s+");
+        ArrayDeque<String> numsAsString = new ArrayDeque<>();
+        
+
+    }
+}
