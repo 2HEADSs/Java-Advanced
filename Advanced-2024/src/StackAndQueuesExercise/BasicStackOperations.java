55d148b68069926a4f68c7fff68d01920b07f7c4
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-15 14:24:27 +0200

    Update BasicStackOperations.java

--- Advanced-2024/src/StackAndQueuesExercise/BasicStackOperations.java
+++ Advanced-2024/src/StackAndQueuesExercise/BasicStackOperations.java
@@ -1,5 +1,6 @@
 package StackAndQueuesExercise;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class BasicStackOperations {
@@ -7,8 +8,41 @@ public class BasicStackOperations {
         Scanner scanner = new Scanner(System.in);
         String [] firstLine = (scanner.nextLine().split(" "));
         int n = Integer.parseInt(firstLine[0]);
-        int s = Integer.parseInt(firstLine[0]);
-        int x = Integer.parseInt(firstLine[0]);
-        
+        int s = Integer.parseInt(firstLine[1]);
+        int x = Integer.parseInt(firstLine[2]);
+
+        String [] secondLine = (scanner.nextLine().split(" "));
+
+        ArrayDeque<Integer> stackOfNums  = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            stackOfNums.push(Integer.parseInt(secondLine[i]));
+        }
+
+        for (int i = 1; i <= s; i++) {
+            stackOfNums.poll();
+        }
+        int minMalue = Integer.MAX_VALUE;
+        boolean present = false;
+        if(stackOfNums.isEmpty()){
+            minMalue = 0;
+        }else {
+            while (!stackOfNums.isEmpty()){
+                int currentNum = stackOfNums.poll();
+                if(currentNum < minMalue){
+                    minMalue = currentNum;
+                }
+                if(currentNum ==x){
+                    present = true;
+                }
+            }
+        }
+
+        if(present){
+            System.out.println(true);
+        }else {
+            System.out.println(minMalue);
+        }
+
     }
 }
