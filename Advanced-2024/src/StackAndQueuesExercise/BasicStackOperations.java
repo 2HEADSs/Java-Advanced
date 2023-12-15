9308c9cdef7cabefbb4ee843501389c61cdd98fb
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-15 14:01:17 +0200

    Create BasicStackOperations.java

---
+++ Advanced-2024/src/StackAndQueuesExercise/BasicStackOperations.java
@@ -0,0 +1,14 @@
+package StackAndQueuesExercise;
+
+import java.util.Scanner;
+
+public class BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] firstLine = (scanner.nextLine().split(" "));
+        int n = Integer.parseInt(firstLine[0]);
+        int s = Integer.parseInt(firstLine[0]);
+        int x = Integer.parseInt(firstLine[0]);
+        
+    }
+}
