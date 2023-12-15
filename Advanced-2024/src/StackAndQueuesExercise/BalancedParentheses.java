e7a6471af293b2d647114d4c8c30f3f79ea711aa
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-15 17:10:20 +0200

    Create BalancedParentheses.java

---
+++ Advanced-2024/src/StackAndQueuesExercise/BalancedParentheses.java
@@ -0,0 +1,56 @@
+package StackAndQueuesExercise;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] line = scanner.nextLine().split("");
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        boolean balanced = false;
+        if (line.length % 2 != 0) {
+            System.out.println("NO");
+            return;
+        }
+        for (int i = 0; i < line.length; i++) {
+            stack.push(line[i]);
+            queue.offer(line[i]);
+        }
+        for (int i = 0; i < line.length / 2; i++) {
+            String currentStackString = stack.pop();
+            String currentQueueString = queue.poll();
+            switch (currentStackString) {
+                case "}":
+                    if (currentQueueString.equals("{")) {
+                        balanced = true;
+                    } else {
+                        balanced = false;
+                    }
+                    break;
+                case "]":
+                    if (currentQueueString.equals("[")) {
+                        balanced = true;
+                    } else {
+                        balanced = false;
+                    }
+                    break;
+                case ")":
+                    if (currentQueueString.equals("(")) {
+                        balanced = true;
+                    } else {
+                        balanced = false;
+                    }
+                    break;
+            }
+        }
+
+        if (balanced) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
