2e9fdd3f37a66fb177540a5a8a5b351d2b14defa
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-15 23:40:06 +0200

    Update Fibonacci.java

--- Advanced-2024/src/StackAndQueuesExercise/Fibonacci.java
+++ Advanced-2024/src/StackAndQueuesExercise/Fibonacci.java
@@ -8,16 +8,18 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int count = Integer.parseInt(scanner.nextLine());
-        if (count ==0){
-            System.out.println(0);
+        if (count ==1){
+            System.out.println(1);
             return;
         }
         Deque<Integer> nums = new ArrayDeque<>();
         nums.push(1);
-        nums.push(1);
-        for (int i = 0; i < count-1; i++) {
+        for (int i = 0; i < count; i++) {
             int firstNum = nums.pop();
-            int secondNum = nums.pop();
+            int secondNum = 0;
+            if(!nums.isEmpty()){
+                secondNum = nums.pop();
+            }
             int thirdNum = firstNum +secondNum;
             nums.push(secondNum);
             nums.push(firstNum);
