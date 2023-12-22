c000e5d7d8389dbd4ebe8ee8c5d8c57631671295
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-22 12:08:56 +0200

    Create MatrixofPalindromes2.java

---
+++ Advanced-2024/src/MultidimensionalArraysExercise/MatrixofPalindromes2.java
@@ -0,0 +1,45 @@
+package MultidimensionalArraysExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MatrixofPalindromes2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] input = readArray(scanner);
+        int r = input[0];
+        int c = input[1];
+
+        String[][] matrix = new String[r][c];
+        fillPalindromeMatrix(matrix);
+        printMatrix(matrix);
+    }
+
+
+    private static void fillPalindromeMatrix(String[][] matrix) {
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                String outsideSymbol = String.valueOf((char) ('a' + row));
+                String midSymbol = String.valueOf((char) ('a' + row + col));
+                matrix[row][col] = outsideSymbol + midSymbol + outsideSymbol;
+            }
+        }
+    }
+
+    private static int[] readArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private static void printMatrix(String[][] matrix) {
+        for (String[] arrOuter : matrix) {
+            for (String arrInner : arrOuter) {
+                System.out.print(arrInner+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
