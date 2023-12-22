480c737d5aa8c8abc1ad707ada7c75f5e292ccc8
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-22 16:18:12 +0200

    Update MaximalSum.java

--- Advanced-2024/src/MultidimensionalArraysExercise/MaximalSum.java
+++ Advanced-2024/src/MultidimensionalArraysExercise/MaximalSum.java
@@ -1,5 +1,6 @@
 package MultidimensionalArraysExercise;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,45 +11,58 @@ public class MaximalSum {
         int rows = sizes[0];
         int cols = sizes[1];
 
-        int[][] matrix = createMatrix(rows, cols, scanner);
-        printMatrix(matrix);
+        int[][] matrix = new int[rows][cols];
+        createMatrix(matrix, scanner);
+
         findMaxSum(matrix);
 
+
     }
 
-    private static int findMaxSum(int[][] matrix){
-
-        int maxSum = 0;
+    private static void findMaxSum(int[][] matrix) {
+        int maxSum = Integer.MIN_VALUE;
+        ArrayDeque<int[][]> maxSumArrays = new ArrayDeque<>();
         int currentSum = 0;
-        int roundsInner = 0;
-        int roundsOutteer = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i; j < matrix[i].length; j++) {
-                if(roundsInner<3){
-                currentSum += matrix[i][j];
+
+        for (int i = 0; i < matrix.length - 2; i++) {
+            int[][] currentArray = new int[3][3];
+
+            for (int j = i; j < matrix[i].length - 2; j++) {
+                currentSum = matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2] +
+                        matrix[i + 1][j] + matrix[i + 1][j + 1] + matrix[i + 1][j + 2] +
+                        matrix[i + 2][j] + matrix[i + 2][j + 1] + matrix[i + 2][j + 2];
+
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+
+                    currentArray[0][0] = matrix[i][j];
+                    currentArray[0][1] = matrix[i][j + 1];
+                    currentArray[0][2] = matrix[i][j + 2];
+                    currentArray[1][0] = matrix[i + 1][j];
+                    currentArray[1][1] = matrix[i + 1][j + 1];
+                    currentArray[1][2] = matrix[i + 1][j + 2];
+                    currentArray[2][0] = matrix[i + 2][j];
+                    currentArray[2][1] = matrix[i + 2][j + 1];
+                    currentArray[2][2] = matrix[i + 2][j + 2];
+                    maxSumArrays.push(currentArray);
                 }
-                roundsInner++;
             }
         }
-                return maxSum;
+        System.out.println("Sum = " + maxSum);
+        printMatrix(maxSumArrays.pop());
     }
+
     private static int[] readArray(Scanner scanner) {
         return Arrays.stream(scanner.nextLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
     }
 
-    private static int[][] createMatrix(int r, int c, Scanner scanner) {
-        int matrix[][] = new int[r][c];
-
-        for (int i = 0; i < r; i++) {
-            int[] line = readArray(scanner);
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = line[j];
-
-            }
+    private static void createMatrix(int[][] matrix, Scanner scanner) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = readArray(scanner);
         }
-        return matrix;
+
     }
 
     private static void printMatrix(int[][] matrix) {
