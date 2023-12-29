e495536751a243385623b28dd9d498c9104c0bb6
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-29 14:03:29 +0200

    Create Phonebook.java

---
+++ Advanced-2024/src/SetsAndMapsAdvancedExercise/Phonebook.java
@@ -0,0 +1,33 @@
+package SetsAndMapsAdvancedExercise;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Phonebook {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> phonebook = new HashMap<>();
+
+        String line = scanner.nextLine();
+        while (!"search".equals(line)) {
+            String[] commands = line.split("-");
+            String name = commands[0];
+            String number = commands[1];
+
+            phonebook.put(name, number);
+            line = scanner.nextLine();
+        }
+        String searchName = scanner.nextLine();
+        while (!"stop".equals(searchName)) {
+            if (phonebook.containsKey(searchName)) {
+                System.out.println(searchName + " -> " + phonebook.get(searchName));
+            } else {
+                System.out.println("Contact " + searchName + " does not exist.");
+            }
+        searchName = scanner.nextLine();
+        }
+
+    }
+}
