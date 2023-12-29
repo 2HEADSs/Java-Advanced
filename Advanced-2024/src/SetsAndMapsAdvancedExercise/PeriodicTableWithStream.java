30802b45aa43fe5559043ddd41130379ab5b701b
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-29 11:51:47 +0200

    Create PeriodicTableWithStream.java

---
+++ Advanced-2024/src/SetsAndMapsAdvancedExercise/PeriodicTableWithStream.java
@@ -0,0 +1,23 @@
+package SetsAndMapsAdvancedExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class PeriodicTableWithStream {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = Integer.parseInt(scanner.nextLine());
+
+        TreeSet<String> elements = new TreeSet<>();
+        for (int i = 0; i < count; i++) {
+        elements.addAll(Arrays.asList(scanner.nextLine().split("\\s+")));
+
+        }
+
+
+        for (String el : elements) {
+            System.out.print(el + " ");
+        }
+    }
+}
