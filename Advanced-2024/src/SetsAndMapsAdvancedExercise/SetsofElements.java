962daf15b9ea921dc783228b2466381342c8483e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-29 11:41:26 +0200

    Update SetsofElements.java

--- Advanced-2024/src/SetsAndMapsAdvancedExercise/SetsofElements.java
+++ Advanced-2024/src/SetsAndMapsAdvancedExercise/SetsofElements.java
@@ -7,30 +7,26 @@ import java.util.Scanner;
 public class SetsofElements {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        LinkedHashSet<Integer> firstNums = new LinkedHashSet<>();
-        LinkedHashSet<Integer> secondNums = new LinkedHashSet<>();
-        LinkedHashSet<Integer> reapeatableNums = new LinkedHashSet<>();
+        LinkedHashSet<String> firstSet = new LinkedHashSet<>();
+        LinkedHashSet<String> secondSet = new LinkedHashSet<>();
 
         String[] inputNums = scanner.nextLine().split("\\s+");
         int firstCount = Integer.parseInt(inputNums[0]);
         int secondCount = Integer.parseInt(inputNums[1]);
 
         while (firstCount-- > 0) {
-            int input = Integer.parseInt(scanner.nextLine());
-            firstNums.add(input);
+            firstSet.add(scanner.nextLine());
         }
+
         while (secondCount-- > 0) {
-            int input = Integer.parseInt(scanner.nextLine());
-            if (firstNums.contains(input)) {
-                reapeatableNums.add(input);
-            }
-            secondNums.add(input);
-
+            secondSet.add(scanner.nextLine());
         }
 
-        for (int e : reapeatableNums) {
-            System.out.print(e + " ");
-        }
+        firstSet.retainAll(secondSet);
+
+        String result = String.join("",firstSet);
+        System.out.println(result);
+
 
     }
 }
