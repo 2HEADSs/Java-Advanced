d3143a8961cb6c5b47760f197c2fa2256b68af12
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-01 22:23:48 +0200

    Create CopyPicture2.java

---
+++ Advanced-2024/src/StreamsFilesAndDirectoriesExercises/CopyPicture2.java
@@ -0,0 +1,27 @@
+package StreamsFilesAndDirectoriesExercises;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyPicture2 {
+    public static void main(String[] args) {
+
+        String inputPath = "C:\\Programing\\Java-Github\\Java-Advanced\\Advanced-2024\\src\\StreamsFilesAndDirectoriesExercises\\pic.jpg";
+        String outputPath = "C:\\Programing\\Java-Github\\Java-Advanced\\Advanced-2024\\src\\StreamsFilesAndDirectoriesExercises\\picture-copy.jpg";
+
+        try (FileInputStream fileInputStream = new FileInputStream(inputPath);
+             FileOutputStream fileOutputStream = new FileOutputStream(outputPath)) {
+
+            int oneByte = fileInputStream.read();
+            while (oneByte !=-1){
+                fileOutputStream.write(oneByte);
+                oneByte = fileInputStream.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
