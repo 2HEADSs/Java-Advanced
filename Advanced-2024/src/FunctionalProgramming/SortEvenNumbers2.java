e064db7fa1e653f7bae08b4d027e5988f21b470b
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-03 23:23:26 +0200

    Create SortEvenNumbers2.java

---
+++ Advanced-2024/src/FunctionalProgramming/SortEvenNumbers2.java
@@ -0,0 +1,35 @@
+package FunctionalProgramming;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SortEvenNumbers2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .filter(e -> e % 2 == 0)
+                .collect(Collectors.toList());
+
+
+        System.out.println(joinNumbers(numbers));
+
+        List<Integer> sortedNumbers = numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(joinNumbers(sortedNumbers));
+    }
+
+    public static String joinNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+    }
+}
+
