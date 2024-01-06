3c804d96255be6a3d0790e756f53844120b4976e
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-06 11:02:10 +0200

    Create AddVAT2.java

---
+++ Advanced-2024/src/FunctionalProgramming/AddVAT2.java
@@ -0,0 +1,21 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public class AddVAT2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Prices with VAT:");
+
+        UnaryOperator<String> priceWithVAT =
+                s -> String.format("%.2f", Double.parseDouble(s) * 1.2);
+
+        Arrays.stream(scanner.nextLine().split(", "))
+                .map(priceWithVAT)
+                .forEach(System.out::println);
+    }
+}
