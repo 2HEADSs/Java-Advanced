89b9dfb7a1c8dee96be69e16550223c4f436ab79
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-10 18:18:13 +0200

    Create AppliedArithmetics.java

---
+++ Advanced-2024/src/FunctionalProgrammingExercise/AppliedArithmetics.java
@@ -0,0 +1,25 @@
+package FunctionalProgrammingExercise;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class AppliedArithmetics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Function<Integer[], Integer> findSmallest = arr -> {
+            return Arrays.stream(arr)
+                    .min(Integer::compare)
+                    .get();
+        };
+        Integer[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
+
+        System.out.println(findSmallest.apply(numbers));
+
+
+
+    }
+}
