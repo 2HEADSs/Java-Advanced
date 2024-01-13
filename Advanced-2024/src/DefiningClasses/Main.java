f549ea47b064388b82e093cac6ed09e4546c5d92
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-13 20:00:01 +0200

    Update Main.java

--- Advanced-2024/src/DefiningClasses/Main.java
+++ Advanced-2024/src/DefiningClasses/Main.java
@@ -1,15 +1,17 @@
 package DefiningClasses;
 
+
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
 
 
-        =car.setBrand("Chevrolet");
+        car.setBrand("Chevrolet");
         car.setModel("Impala");
         car.setHorsePower(390);
 
-        System.out.printf("The car is: %s %s - %d HP",
-                car.brand, car.model, car.horsePower);
+        System.out.printf("The car is: %s %s - %d HP%n",
+                car.getBrand(), car.getModel(), car.getHorsePower());
+        System.out.println(car.toString());
     }
 }
