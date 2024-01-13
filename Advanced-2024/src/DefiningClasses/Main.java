ef0305ed7d4431c350e5448ea82b4f8735cff29f
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-13 21:12:38 +0200

    Update Main.java

--- Advanced-2024/src/DefiningClasses/Main.java
+++ Advanced-2024/src/DefiningClasses/Main.java
@@ -1,17 +1,33 @@
 package DefiningClasses;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
+        Scanner scanner = new Scanner(System.in);
 
+        List<Car> cars = new ArrayList<>();
 
-        car.setBrand("Chevrolet");
-        car.setModel("Impala");
-        car.setHorsePower(390);
+        int n = Integer.parseInt(scanner.nextLine());
+        while (n-- > 0) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+            String brand = tokens[0];
+            Car car;
 
-        System.out.printf("The car is: %s %s - %d HP%n",
-                car.getBrand(), car.getModel(), car.getHorsePower());
-        System.out.println(car.toString());
+            if (tokens.length == 1) {
+                car = new Car(brand);
+            } else {
+                String model = tokens[1];
+                int horsePower = Integer.parseInt(tokens[2]);
+                car = new Car(brand, model, horsePower);
+            }
+            cars.add(car);
+        }
+
+        cars.forEach(System.out::println);
+
     }
 }
