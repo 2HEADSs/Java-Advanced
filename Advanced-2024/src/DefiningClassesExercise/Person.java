0063a592c9bd70f6b311fe8ee5533c12413b0fab
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-14 14:15:12 +0200

    Create Person.java

---
+++ Advanced-2024/src/DefiningClassesExercise/Person.java
@@ -0,0 +1,36 @@
+package DefiningClassesExercise;
+
+public class Person {
+
+    public String name;
+    public int years;
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setYears(int years) {
+//        this.years = years;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getYears() {
+//        return years;
+//    }
+
+
+    public Person(String name, int years) {
+        this.name = name;
+        this.years = years;
+    }
+
+    static class PersonComparator implements java.util.Comparator<Person> {
+        @Override
+        public int compare(Person a, Person b) {
+            return b.years - a.years;
+        }
+    }
+}
