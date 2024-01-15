c3f44abb892b875788b8259bbc03d6e512fcc112
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-15 23:29:33 +0200

    Update Cargo.java

--- Advanced-2024/src/DefiningClassesExercise/RawData/Cargo.java
+++ Advanced-2024/src/DefiningClassesExercise/RawData/Cargo.java
@@ -7,6 +7,13 @@ public class Cargo {
     public Cargo(int cargoWeight, String cargoType){
         this.cargoWeight = cargoWeight;
         this.cargoType = cargoType;
+    }
 
+    public int getCargoWeight() {
+        return cargoWeight;
+    }
+
+    public String getCargoType() {
+        return cargoType;
     }
 }
