48719f2f1401e4ee1e3b37c1dfe7b12771b32665
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-15 23:29:29 +0200

    Update Main.java

--- Advanced-2024/src/DefiningClassesExercise/RawData/Main.java
+++ Advanced-2024/src/DefiningClassesExercise/RawData/Main.java
@@ -1,5 +1,52 @@
 package DefiningClassesExercise.RawData;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Car> cars = new ArrayList<>();
+        while (n-- > 0) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+            String model = tokens[0];
+            int engineSpeed = Integer.parseInt(tokens[1]);
+            int enginePower = Integer.parseInt(tokens[2]);
+            int cargoWeight = Integer.parseInt(tokens[3]);
+            String cargoType = tokens[4];
+            double tire1Pressure = Double.parseDouble(tokens[5]);
+            int tire1aAge = Integer.parseInt(tokens[6]);
+            double tire2Pressure = Double.parseDouble(tokens[7]);
+            int tire2aAge = Integer.parseInt(tokens[8]);
+            double tire3Pressure = Double.parseDouble(tokens[9]);
+            int tire3aAge = Integer.parseInt(tokens[10]);
+            double tire4Pressure = Double.parseDouble(tokens[11]);
+            int tire4aAge = Integer.parseInt(tokens[12]);
+            Car car = new Car(model, engineSpeed, enginePower, cargoWeight, cargoType,
+                    tire1Pressure, tire1aAge,
+                    tire2Pressure, tire2aAge,
+                    tire3Pressure, tire3aAge,
+                    tire4Pressure, tire4aAge);
+            cars.add(car);
+        }
+
+        String filterType = scanner.nextLine();
+        if (filterType.equals("fragile")) {
+            cars.stream()
+                    .filter(car -> car.getCargo().getCargoType().equals("fragile") &&
+                            car.getTires().stream().anyMatch(tire -> tire.getPressure() < 1))
+                    .forEach(car -> System.out.println(car.getModel()));
+        } else {
+            cars.stream()
+                    .filter(car -> car.getCargo().getCargoType().equals("flamable") &&
+                            car.getEngine().getEnginePower() > 250)
+                    .forEach(car -> System.out.println(car.getModel()));
+        }
+
+    }
 }
