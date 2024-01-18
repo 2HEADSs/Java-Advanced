b4e960ca46894d7f02652034f60ab7f0f4e537fb
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-18 22:59:02 +0200

    Create Car.java

---
+++ Advanced-2024/src/DefiningClassesExercise/SpeedRacing/Car.java
@@ -0,0 +1,66 @@
+package DefiningClassesExercise.SpeedRacing;
+
+public class Car {
+
+    String model;
+    double fuelAmount;
+
+    double fuelCost;
+
+    int distance;
+
+
+    public Car(String model, double fuelAmount, double fuelCost) {
+        this.model = model;
+        this.fuelAmount = fuelAmount;
+        this.fuelCost = fuelCost;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public double getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public double getFuelCost() {
+        return fuelCost;
+    }
+
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setFuel(double fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+    public void sumDistance(int km){
+        setDistance(km + getDistance());
+    }
+
+    public void setFuelCost(double fuelCost) {
+        this.fuelCost = fuelCost;
+    }
+
+    public boolean tryToDrive(int km) {
+
+        double leftAmount = (this.getFuelAmount() - (this.getFuelCost() * km));
+        if (leftAmount > 0) {
+            this.setFuel(leftAmount);
+            sumDistance(km);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
