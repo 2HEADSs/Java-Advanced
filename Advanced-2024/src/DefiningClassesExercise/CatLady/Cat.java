7ba814889d18fb569f1893f2b1af7c74fc1484ce
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-19 23:21:42 +0200

    Create Cat.java

---
+++ Advanced-2024/src/DefiningClassesExercise/CatLady/Cat.java
@@ -0,0 +1,27 @@
+package DefiningClassesExercise.CatLady;
+
+public class Cat {
+    private String name;
+
+    private String breed;
+
+    public Cat(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", this.breed, this.name);
+    }
+}
+
+
