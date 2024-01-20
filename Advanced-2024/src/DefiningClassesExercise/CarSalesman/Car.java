e2b14fc7d26df1c38245777e4317f7e17b622139
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-20 12:40:51 +0200

    Update Car.java

--- Advanced-2024/src/DefiningClassesExercise/CarSalesman/Car.java
+++ Advanced-2024/src/DefiningClassesExercise/CarSalesman/Car.java
@@ -2,9 +2,64 @@ package DefiningClassesExercise.CarSalesman;
 
 public class Car {
     private String model;
-    private String engine;
+    private Engine engine;
 
     private int weight;
 
     private String color;
+
+
+    public Car(String model, Engine engine) {
+        this(model, engine, 0, "n/a");
+    }
+
+    public Car(String model, Engine engine, int weight) {
+        this(model, engine, weight, "n/a");
+    }
+
+    public Car(String model, Engine engine, String color) {
+        this(model, engine, 0, color);
+    }
+
+
+    public Car(String model, Engine engine, int weight, String color) {
+        this.model = model;
+        this.engine = engine;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        String currentWeight = "n/a";
+        if (getWeight() > 0) {
+            currentWeight = String.valueOf(getWeight());
+        }
+
+        String currentDisplacement = "n/a";
+        if (engine.getDisplacement() > 0) {
+            currentDisplacement = String.valueOf(engine.getDisplacement());
+        }
+
+        return String.format("%s:%n%s:%nPower: %d%nDisplacement: %s%nEfficiency: %s%nWeight: %s%nColor: %s",
+                getModel(), engine.getModel(), engine.getPower(), currentDisplacement,
+                engine.getEfficiency(), currentWeight, getColor());
+    }
 }
+
