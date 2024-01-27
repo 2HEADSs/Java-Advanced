304dc68e4f50ca172335f43936178d6511e60ec4
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-27 22:55:17 +0200

    Update Main.java

--- Advanced-2024/src/Main.java
+++ Advanced-2024/src/Main.java
@@ -7,12 +7,13 @@ public class Main {
         Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
         Book bookThree = new Book("The Documents in the Case", 2002);
 
-        Library library = new Library(bookOne, bookTwo, bookThree);
-
-        for (Book book : library) {
-            System.out.println(book.getTitle());
+        if (bookOne.compareTo(bookTwo) > 0) {
+            System.out.println(String.format("%s is before %s", bookOne, bookTwo));
+        } else if (bookOne.compareTo(bookTwo) < 0) {
+            System.out.println(String.format("%s is before %s", bookTwo, bookOne));
+        } else {
+            System.out.println("Book are equal");
         }
-
-
     }
+
 }
