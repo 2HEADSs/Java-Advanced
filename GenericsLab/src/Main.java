1e8f193c0e78dfa8cfd806ceccc19c4ad5514716
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-27 13:29:33 +0200

    Update Main.java

--- GenericsLab/src/Main.java
+++ GenericsLab/src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+//        String[] nones = ArrayCreator.create(10, "none");
+//
+//        System.out.println(nones);
+
+        Scale<String> scale = new Scale<>("a","b");
+        Scale<Integer> scale2 = new Scale<>(1,2);
+
+        System.out.println(scale.getHeavier());
+        System.out.println(scale2.getHeavier());
+    }
 }
